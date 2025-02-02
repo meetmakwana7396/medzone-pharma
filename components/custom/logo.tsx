b67ab513ef6d5284ac1794b17fb69f82aa117398
meetmakwana7396 <makwnameet7301@gmail.com>
2025-02-03 01:53:02 +0530

    small change

--- components/custom/logo.tsx
+++ components/custom/logo.tsx
@@ -1,15 +1,18 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 export default function Logo({ className }: { className?: string }) {
   return (
-    <Image
-      src="/images/logo.png"
-      alt="logo"
-      width={200}
-      height={156}
-      priority
-      className={className}
-    />
+    <Link href="/">
+      <Image
+        src="/images/logo.png"
+        alt="logo"
+        width={200}
+        height={156}
+        priority
+        className={className}
+      />
+    </Link>
   );
 }
